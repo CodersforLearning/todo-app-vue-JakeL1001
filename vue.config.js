377b039ce899c05754723465714e5a7b4844e4bd
JakeL1001 <50351260+JakeL1001@users.noreply.github.com>
2022-01-15 01:00:45 +0800

    Update Git Link

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/todo-app-vue-JakeL1001/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/todo-app-vue-JakeLDev/" : "/",
 };
